cf0d817c15618567d329023e0193cf3f6f0c6a2d
Mohd Jeeshan <jeeshanmohammad921@gmail.com>
2025-09-24 23:39:25 +0530

    connect with database

---
+++ src/index.js
@@ -0,0 +1,27 @@
+
+import connectDB from "./db/index.js";
+import dotenv from "dotenv"
+  
+
+dotenv.config({
+    path:'./env'
+})
+connectDB()
+
+/*
+import express from "express";
+( async ()=>{
+    try {
+        mongoose,connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+        app.on("error",(error)=>{
+            console.log("ERROR",error);
+            throw error
+        })  
+        app.listen(process.env.PORT,()=>{
+            console.log(`app is listening ${process,env.PORT}`); 
+        }) 
+    } catch (error) {
+        console.error("ERROR",error);
+        
+    }
+})()*/
